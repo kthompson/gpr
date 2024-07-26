fbafb3a80c47949d55973d915c838f0ddd95154f
Kevin Thompson <kevin@theautomaters.com>
2024-07-26 00:10:52 -0700

    implement status

--- src/gpr/Commands/StatusCommand.cs
+++ src/gpr/Commands/StatusCommand.cs
@@ -20,7 +20,50 @@ public class StatusCommandHandler(IAnsiConsole console, IRepository repo)
         CancellationToken cancellationToken
     )
     {
-        // show the current file listing
+        var status = repo.RetrieveStatus(new StatusOptions
+        {
+            Show = StatusShowOption.IndexAndWorkDir,
+            DetectRenamesInIndex = true,
+            DetectRenamesInWorkDir = true,
+        });
+        
+        foreach (var entry in status)
+        {
+            var state = entry.State;
+            
+            var ignored = state == FileStatus.Ignored;
+            if(ignored) continue;
+            
+            var renamed = (state & (FileStatus.RenamedInIndex | FileStatus.RenamedInWorkdir)) != 0;
+            if (renamed)
+            {
+                console.MarkupLine($"[red3_1]R {entry.FilePath}[/]");
+                continue;
+            }
+            
+            var modified = (state & (FileStatus.ModifiedInIndex | FileStatus.ModifiedInWorkdir)) != 0;
+            if(modified)
+            {
+                console.MarkupLine($"[dodgerblue2]M {entry.FilePath}[/]");
+                continue;
+            }
+            
+            var untracked = (state & (FileStatus.NewInWorkdir)) != 0;
+            if(untracked)
+            {
+                console.MarkupLine($"[hotpink2][underline]? {entry.FilePath}[/][/]");
+                continue;
+            }
+
+            var added = (state & FileStatus.NewInIndex) == FileStatus.NewInIndex;
+            if(added)
+            {
+                console.MarkupLine($"[green]A {entry.FilePath}[/]");
+                continue;
+            }
+            
+            throw new ArgumentOutOfRangeException(entry.State + ": " + entry.FilePath);
+        }
         return 0;
     }
 }
